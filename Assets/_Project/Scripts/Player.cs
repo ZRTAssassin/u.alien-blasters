1077c681b9e1ec9f05e759170d44ea3cd55a323b
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-02-15 15:25:28 -0600

    Added horizontal movement

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal");
+        // var vertical = Input.GetAxis("Vertical");
+        Debug.Log($"{horizontal}");
+        _rb.velocity = new Vector2(horizontal, _rb.velocity.y);
     }
 }
