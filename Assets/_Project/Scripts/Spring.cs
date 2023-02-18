d35b9d163996683f49320b55f6f09b257f8fa344
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-02-18 17:20:15 -0600

    Added audio to spring

--- Assets/_Project/Scripts/Spring.cs
+++ Assets/_Project/Scripts/Spring.cs
@@ -9,10 +9,14 @@ namespace _Project.Scripts
         Sprite _defaultSprite;
         [SerializeField] Sprite _sprungSprite;
 
+        [Header("Audio"), Space(5)] [SerializeField]
+        AudioSource _audioSource;
+
         void Awake()
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _defaultSprite = _spriteRenderer.sprite;
+            _audioSource = GetComponent<AudioSource>();
         }
 
         void OnCollisionEnter2D(Collision2D other)
@@ -20,6 +24,7 @@ namespace _Project.Scripts
             if (other.collider.CompareTag("Player"))
             {
                 _spriteRenderer.sprite = _sprungSprite;
+                _audioSource.Play();
             }
 
         }
