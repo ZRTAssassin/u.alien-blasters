06d4b43912a0d6923cdcbee4c3a10719158dbde0
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-02-21 10:20:32 -0600

    Added frog graphics

--- Assets/_Project/Scripts/Frog.cs
+++ Assets/_Project/Scripts/Frog.cs
@@ -9,17 +9,30 @@ public class Frog : MonoBehaviour
     [SerializeField] SpriteRenderer _spriteRenderer;
     [SerializeField] float _jumpDelay = 3.0f;
     [SerializeField] Vector2 _jumpForce;
+    
+    [Header("Sprite Setup"), Space(5)]
+    Sprite _defaultSprite;
+    [SerializeField] Sprite _jumpSprite;
 
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
+        _defaultSprite = _spriteRenderer.sprite;
+        InvokeRepeating(nameof(Jump), _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
         _rb.AddForce(_jumpForce);
+        _jumpForce *= new Vector2(-1, 1);
+        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        _spriteRenderer.sprite = _jumpSprite;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        _spriteRenderer.sprite = _defaultSprite;
     }
 }
