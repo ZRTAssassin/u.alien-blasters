fdf609e4eb18d3df0ad50525286f7d715d530c90
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-02-21 10:23:26 -0600

    Fixed some collider weirdness

--- Assets/_Project/Scripts/Frog.cs
+++ Assets/_Project/Scripts/Frog.cs
@@ -1,38 +1,38 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Frog : MonoBehaviour
+namespace _Project.Scripts
 {
-    [SerializeField] Rigidbody2D _rb;
-    [SerializeField] SpriteRenderer _spriteRenderer;
-    [SerializeField] float _jumpDelay = 3.0f;
-    [SerializeField] Vector2 _jumpForce;
+    public class Frog : MonoBehaviour
+    {
+        [SerializeField] Rigidbody2D _rb;
+        [SerializeField] SpriteRenderer _spriteRenderer;
+        [SerializeField] float _jumpDelay = 3.0f;
+        [SerializeField] Vector2 _jumpForce;
     
-    [Header("Sprite Setup"), Space(5)]
-    Sprite _defaultSprite;
-    [SerializeField] Sprite _jumpSprite;
+        [Header("Sprite Setup"), Space(5)]
+        Sprite _defaultSprite;
+        [SerializeField] Sprite _jumpSprite;
 
 
-    void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _defaultSprite = _spriteRenderer.sprite;
-        InvokeRepeating(nameof(Jump), _jumpDelay, _jumpDelay);
-    }
+        void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _defaultSprite = _spriteRenderer.sprite;
+            InvokeRepeating(nameof(Jump), _jumpDelay, _jumpDelay);
+        }
 
-    void Jump()
-    {
-        _rb.AddForce(_jumpForce);
-        _jumpForce *= new Vector2(-1, 1);
-        _spriteRenderer.flipX = !_spriteRenderer.flipX;
-        _spriteRenderer.sprite = _jumpSprite;
-    }
+        void Jump()
+        {
+            _rb.AddForce(_jumpForce);
+            _jumpForce *= new Vector2(-1, 1);
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+            _spriteRenderer.sprite = _jumpSprite;
+        }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        _spriteRenderer.sprite = _defaultSprite;
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            _spriteRenderer.sprite = _defaultSprite;
+        }
     }
 }
