b9ae8839b7cdab8c965d8407277e3347ca84e01b
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-03-29 18:30:12 -0500

    added breakable brick

---
+++ Assets/_Project/Scripts/Brick.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player == null)
+        {
+            return;
+        }
+
+        Vector2 normal = other.contacts[0].normal;
+        float dot = Vector2.Dot(normal, Vector2.up);
+        Debug.Log(dot);
+        if (dot > 0.5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/_Project/Scripts/MovingPlatform.cs
+++ Assets/_Project/Scripts/MovingPlatform.cs
@@ -8,7 +8,7 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] List<Vector3> _positions = new List<Vector3>();
     [SerializeField] Vector3 _position1;
     [SerializeField] Vector3 _position2;
-    [Range(0f, 10f)] [SerializeField] float _platformMoveSpeed = 10f;
+    [Range(0f, 2f)] [SerializeField] float _platformMoveSpeed = 1f;
     [Range(0f, 1f)] [SerializeField] float _percentAcross;
 
     // Update is called once per frame
