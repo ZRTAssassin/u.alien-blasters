2cc6e68f5644860e83e0726435b556adac48b031
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-03-29 18:25:01 -0500

    dev level working

--- Assets/_Project/Scripts/MovingPlatform.cs
+++ Assets/_Project/Scripts/MovingPlatform.cs
@@ -8,11 +8,13 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] List<Vector3> _positions = new List<Vector3>();
     [SerializeField] Vector3 _position1;
     [SerializeField] Vector3 _position2;
+    [Range(0f, 10f)] [SerializeField] float _platformMoveSpeed = 10f;
     [Range(0f, 1f)] [SerializeField] float _percentAcross;
 
     // Update is called once per frame
     void Update()
     {
+        _percentAcross = Mathf.PingPong(Time.time * _platformMoveSpeed, 1f);
         transform.position = Vector3.Lerp(_position1, _position2, _percentAcross);
     }
 
@@ -22,7 +24,7 @@ public class MovingPlatform : MonoBehaviour
         var collider = GetComponent<BoxCollider2D>();
         Gizmos.DrawWireCube(_position1, collider.bounds.size);
         Gizmos.DrawWireCube(_position2, collider.bounds.size);
-        
+
         Gizmos.color = Color.yellow;
         var currentPosiotn = Vector3.Lerp(_position1, _position2, _percentAcross);
         Gizmos.DrawWireCube(currentPosiotn, collider.bounds.size);
@@ -30,6 +32,24 @@ public class MovingPlatform : MonoBehaviour
 
     [ContextMenu("SetPosition1")]
     public void SetPosition1() => _position1 = transform.position;
+
     [ContextMenu("SetPosition2")]
     public void SetPosition2() => _position1 = transform.position;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.transform.SetParent(transform);
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.transform.SetParent(null);
+        }
+    }
 }
