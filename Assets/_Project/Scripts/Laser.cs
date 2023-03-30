c617c69e8ce77ac9522a97b6c6882b6ec6142ed9
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-03-30 18:50:44 -0500

    laser stops at first target hit

--- Assets/_Project/Scripts/Laser.cs
+++ Assets/_Project/Scripts/Laser.cs
@@ -7,11 +7,27 @@ public class Laser : MonoBehaviour
 {
     LineRenderer _lineRenderer;
     [SerializeField] bool _isOn;
+    [SerializeField] Vector2 _direction = Vector2.left;
+    [SerializeField] float _distance = 10f;
 
     void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
         Toggle(false);
+    }
+
+    void Update()
+    {
+        if (!_isOn)
+            return;
+        var endpoint = (Vector2)transform.position + (_direction * _distance);
+
+        var firstThing = Physics2D.Raycast(transform.position, _direction, _distance);
+        if (firstThing.collider)
+        {
+            endpoint = firstThing.point;
+        }
+        _lineRenderer.SetPosition(1, endpoint);
     }
 
     public void Toggle(bool state)
