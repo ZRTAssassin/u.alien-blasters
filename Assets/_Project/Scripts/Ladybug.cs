eeef12cc30df63e982abbaa53f1e5f74f4ece0f3
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-03-31 18:46:53 -0500

    added downward ray gizmo

--- Assets/_Project/Scripts/Ladybug.cs
+++ Assets/_Project/Scripts/Ladybug.cs
@@ -22,11 +22,24 @@ public class Ladybug : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        
+        var collider = GetComponent<Collider2D>();
         Gizmos.color = Color.red;
-        Vector2 offset = _direction * GetComponent<Collider2D>().bounds.extents.x;
+
+        Vector2 offset = _direction * collider.bounds.extents.x;
         Vector2 origin = (Vector2)transform.position + offset;
         Gizmos.DrawLine(origin, origin + (_direction * _raycastDistance));
+
+        var bounds = collider.bounds;
+        if (_direction == Vector2.left)
+        {
+            Vector2 bottomLeft = new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y);
+            Gizmos.DrawLine(bottomLeft, bottomLeft + (Vector2.down * _raycastDistance));
+        }
+        else
+        {
+            Vector2 bottomRight = new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y);
+            Gizmos.DrawLine(bottomRight, bottomRight + (Vector2.down * _raycastDistance));
+        }
     }
 
     void Update()
@@ -35,7 +48,7 @@ public class Ladybug : MonoBehaviour
         Vector2 origin = (Vector2)transform.position + offset;
 
         var hits = Physics2D.RaycastAll(origin, _direction, _raycastDistance);
-        
+
 
         foreach (var hit in hits)
         {
@@ -46,10 +59,8 @@ public class Ladybug : MonoBehaviour
                 break;
             }
         }
-        
-        
+
+
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
     }
-    
-    
 }
