375796007571439d327d58c1830a10609e4d898a
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-04-02 15:23:39 -0500

    Added bounce to spring

---
+++ Assets/_Project/Scripts/BouncePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BouncePlayer : MonoBehaviour
+{
+    [SerializeField] bool _onlyFromTop;
+    [SerializeField] float _bounciness = 200f;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (_onlyFromTop && Vector2.Dot(other.contacts[0].normal, Vector2.down) < 0.5f)
+        {
+            return;
+        }
+
+        if (other.collider.CompareTag("Player"))
+        {
+            var player = other.collider.GetComponent<Player>();
+            if (player)
+            {
+                player.Bounce(other.contacts[0].normal, _bounciness);
+            }
+        }
+    }
+}
