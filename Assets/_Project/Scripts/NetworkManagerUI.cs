dae884e6744093e3e08d97dbf9eda862d05a07c5
ZRTAssassin <36104436+ZRTAssassin@users.noreply.github.com>
2023-07-28 12:10:38 -0500

    pre relay implementation

--- Assets/_Project/Scripts/NetworkManagerUI.cs
+++ Assets/_Project/Scripts/NetworkManagerUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -11,21 +12,29 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] Button _hostButton;
     [SerializeField] Button _clientButton;
     [SerializeField] GameObject _uiHolder;
+    [SerializeField] UnityTransport _transport;
 
     void Awake()
     {
-        _hostButton.onClick.AddListener(() => { NetworkManager.Singleton.StartHost(); });
+        _hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
         _clientButton.onClick.AddListener(() => { NetworkManager.Singleton.StartClient(); });
+        _transport = GetComponent<UnityTransport>();
     }
 
     void Start()
     {
+        
         NetworkManager.Singleton.OnServerStarted += Singleton_ServerStarted;
         NetworkManager.Singleton.OnClientStarted += Singleton_ServerStarted;
     }
 
     void Singleton_ServerStarted()
     {
+        Debug.Log(
+            $"{_transport.ConnectionData.Address}, {_transport.ConnectionData.Port}, {_transport.ConnectionData.ServerListenAddress}, {_transport.ConnectionData.ListenEndPoint}, {_transport.ConnectionData.ServerEndPoint}");
         _uiHolder.SetActive(false);
     }
 }
